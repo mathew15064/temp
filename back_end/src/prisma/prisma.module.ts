188914884ac616379c7774c5efa2c4c07217b288
[Lampart] Truong Trung Nguyen <trung_nguyen@lampart-vn.com>
2025-12-12 18:03:27 +0800

    create main hehe

--- back_end/src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // Makes PrismaService available everywhere without importing
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
--- back_end/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import {  UsersService } from './users.service';
-import { UsersRouter } from './users.router'; 
-import { PrismaService } from 'src/prisma/prisma.service';
-
-@Module({
-  providers: [PrismaService, UsersService, UsersRouter],
-})
-export class UsersModule {}
